db09944425a340a75d8cb58e90bd16ac7d4ca740
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-06-15 16:20:33 +0200

    Add Alert

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+         
+            Text("Welcome to my tutoriel!")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.green)
+                Button(action: {
+                    print("Button pressed!")
+                    self.alertIsVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+                }
+                .alert(isPresented: $alertIsVisible) { () -> Alert in
+                    return Alert(title: Text("Hello There"), message: Text("This is may first pop-up"), dismissButton: .default(Text("Awsome!")))
+            }
+        }
+           
     }
 }
 
